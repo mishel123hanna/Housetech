8fe929358845a69f3c0d6d3e56bc34be58ba0ba3
Mishel Hanna <mishelhanna3@gmail.com>
2025-11-04 17:01:54 +0300

    convert ALLOWED_HOSTS to str

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env("SECRET_KEY")
 # False if not in os.environ because of casting above
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = str(os.environ.get("ALLOWED_HOSTS")).split(" ")
 
 # Application definition
 
